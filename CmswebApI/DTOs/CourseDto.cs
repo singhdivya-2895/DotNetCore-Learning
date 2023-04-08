1f1c684737f2445066ddb22f0cb87eb22cc760d8
singhdivya-2895 <129833279+singhdivya-2895@users.noreply.github.com>
2023-04-08 15:43:06 +1000

    Adding validation to Dto using attributes (#10)

--- CmswebApI/DTOs/CourseDto.cs
+++ CmswebApI/DTOs/CourseDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -9,10 +10,14 @@ namespace CmswebApI.DTOs
     public class CourseDto
     {
         public int CourseID { get; set; }
+        [Required]
+        [MaxLength(25)]
         public string CourseName { get; set; }
+        [Required]
+        [Range(1,5)]
         public int CourseDuration { get; set; }
         public string Source { get; } = "I am from Dto";
-        
+         [Required]
          [JsonConverter(typeof(JsonStringEnumConverter))]
         public COURSE_TYPE CourseType { get; set; }
     }
