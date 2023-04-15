9c471466c9022a473e5fbb1eb16ff4ccfaff0b42
Pankaj Chaudhary <pankaj.chaudhary@fenergo.com>
2023-04-15 17:31:05 +1000

    Trying more coverage

--- test/CmsWebApi.Test/Controllers/CoursesControllerTest.cs
+++ test/CmsWebApi.Test/Controllers/CoursesControllerTest.cs
@@ -5,19 +5,32 @@ using FluentAssertions;
 using Moq;
 using Xunit;
 using CmswebApI.Controllers;
+using CmswebApI.Repository.Repositories;
 
 namespace CmsWebApi.Test.Controllers
 {
     public class CoursesControllerTests
     {
         [Fact]
-        public void Throws_Exception()
+        public void Constructor_Throws()
         {
             // Arrange
             // Act 
-            // Assert
             Action act = () => _ = new CoursesController(null);
+            // Assert
             act.Should().Throw<ArgumentNullException>("Because the Icmsrepository was not supplied");
+        }
+
+        
+        [Fact]
+        public void Constructor_Success()
+        {
+            // Arrange            
+            var cmsRepository = new Mock<ICmsrepository>();
+            // Act 
+            Action act = () => _ = new CoursesController(cmsRepository.Object);
+            // Assert
+            act.Should().NotThrow<ArgumentNullException>();
         }
     }
 }
